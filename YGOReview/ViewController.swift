8d48bc07ddb4429994b2a34c78f49e202694d54c
omniFatal <matthew.t.karyadi@gmail.com>
2022-05-26 15:59:36 -0700

    Initial Commit

---
+++ YGOReview/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YGOReview
+//
+//  Created by Matthew Karyadi on 5/26/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
