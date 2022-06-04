732f5b2c3f3d927e28d3471924588df1265254a1
omniFatal <matthew.t.karyadi@gmail.com>
2022-06-03 22:28:09 -0700

    Set up table with dummy data

--- YGOReview/SignedIn.swift
+++ YGOReview/SignedIn.swift
@@ -7,12 +7,40 @@
 
 import Foundation
 import UIKit
-class SignedIn: UIViewController {
-    public var userType = ""
 
+class TableViewDelegateAndDataSource: NSObject, UITableViewDataSource, UITableViewDelegate {
+    
+    var data = ["H - Heated Heart", "O - Oversoul", "W - Wing Catapault"]
+    // Return the number of rows for the table.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    // Provide a cell object for each row.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // Fetch a cell of the appropriate type.
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        // Configure the cell’s contents.
+        cell.textLabel!.text = data[indexPath.row]
+        
+        return cell
+    }
+}
+
+
+class SignedIn: UIViewController {
+    
+    @IBOutlet weak var table: UITableView!
+    
+    public var userType = ""
+    var dataSourceAndDelegate = TableViewDelegateAndDataSource()
+    
     override func viewDidLoad() {
         print(userType)
         super.viewDidLoad()
+        table.delegate = dataSourceAndDelegate
+        table.dataSource = dataSourceAndDelegate
     }
-
+    
 }
