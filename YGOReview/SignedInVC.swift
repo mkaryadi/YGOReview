2dd9718e7dbbf2e8d89fd8c0e926414b2116129a
Ramiro Steinmann Petrasso <ramirost@uw.edu>
2022-06-06 16:23:43 -0700

    sorting by rating works

--- YGOReview/SignedInVC.swift
+++ YGOReview/SignedInVC.swift
@@ -97,8 +97,9 @@ class SignedInVC: UIViewController {
         } else {
             sender.title = "Sort Alphabetically"
             // TODO: Sort by rating
-            self.dataSourceAndDelegate.filteredData = []
-            self.table.reloadData()
+            getListOfCardsByRating()
+            //self.dataSourceAndDelegate.filteredData = []
+            //self.table.reloadData()
             sortType = true
         }
     }
@@ -131,6 +132,48 @@ class SignedInVC: UIViewController {
                     print(dict)
                     let actualObject = dict["data"] as? [[String: Any]]
                     for info in actualObject! {
+                        let cardName = (info["name"] as! String)
+                        cardNameList.append(cardName)
+                    }
+                    DispatchQueue.main.async { [self] in
+                        for card in cardNameList {
+                            print(card)
+                        }
+                        self.dataSourceAndDelegate.data = cardNameList
+                        dataSourceAndDelegate.filteredData = cardNameList
+                        self.alphaList = cardNameList
+                        self.table.delegate = dataSourceAndDelegate
+                        self.table.dataSource = dataSourceAndDelegate
+                        self.table.reloadData()
+                    }
+                }
+            }
+            catch {
+                print("Something went boom")
+            }
+        }
+        session.resume()
+    }
+    
+    func getListOfCardsByRating() {
+        var cardNameList: [String] = []
+        var url = URL(string: "https://desolate-ridge-13493.herokuapp.com/cardsbyRating")!
+        let session = URLSession.shared.dataTask(with: url) {
+            data, response, error in
+            
+            if response != nil {
+                if (response! as! HTTPURLResponse).statusCode != 200 {
+                    print("Something went wrong! \(error)")
+                }
+            }
+            
+            let httpResponse = response! as! HTTPURLResponse
+            
+            do {
+                let json = try JSONSerialization.jsonObject(with: data!)
+                print(json)
+                if let actualObject = json as? [[String: Any]] {
+                    for info in actualObject {
                         let cardName = (info["name"] as! String)
                         cardNameList.append(cardName)
                     }
