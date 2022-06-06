98bb177a9c58c4f9aa5fdd9be6da3554e1968fca
Ramiro Steinmann Petrasso <ramirost@uw.edu>
2022-06-05 20:08:07 -0700

    added comment posting function

--- YGOReview/DataRepository.swift
+++ YGOReview/DataRepository.swift
@@ -13,6 +13,7 @@ class DataRepository: NSObject {
     private let ALL_REVIEWS_BY_USER_URL = "https://desolate-ridge-13493.herokuapp.com/allReviewsByUser?author="
     private let ALL_REVIEWS_BY_CARD_URL = "https://desolate-ridge-13493.herokuapp.com/allReviewsByCard?card="
     private let POST_REVIEW_URL = "https://desolate-ridge-13493.herokuapp.com/uploadReview?card="
+    private let POST_COMMENT_URL = "https://desolate-ridge-13493.herokuapp.com/makeComment?reviewCard="
     
     func getAllReviewsByUser(_ user: String) {
         let encodedUser = user.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!
@@ -121,6 +122,64 @@ class DataRepository: NSObject {
     
     func postReview(_ user: String!, _ cardName: String!, numStars: Int, description: String!) {
         var urlString = POST_REVIEW_URL + cardName.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)! + "&author=" + user.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)! + "&numStars=" + String(numStars) + "&description=" + description.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!
+        print(urlString)
+        let url = URL(string: urlString)!
+        let session = URLSession.shared.dataTask(with: url) {
+            data, response, error in
+            
+            if response != nil {
+                if (response! as! HTTPURLResponse).statusCode != 200 {
+                    print("Something went wrong! \(error)")
+                }
+            }
+            
+            let httpResponse = response! as! HTTPURLResponse
+            
+            print(data)
+            
+            do {
+                let json = try JSONSerialization.jsonObject(with: data!)
+                print(json)
+                //        if let subjects = json as? [Any] {
+                //            for subject in subjects {
+                //                if let dictionary = subject as? [String : Any] {
+                //                    let title = dictionary["title"] as? String
+                //                    let description = dictionary["desc"] as? String
+                //                    if let questions = dictionary["questions"] as? [Any] {
+                //                        var questionList : [String] = []
+                //                        var choicesList : [[String]] = []
+                //                        var answerIndexList : [Int] = []
+                //                        for question in questions {
+                //                            if let questionDictionary = question as? [String : Any] {
+                //                                let answerIndex = questionDictionary["answer"] as? String
+                //                                let questionText = questionDictionary["text"] as? String
+                //                                let choices = questionDictionary["answers"] as? [String]
+                //                                print("\(title) \(answerIndex)")
+                ////                                answerIndexList.append(answerIndex!)
+                ////                                choicesList.append(choices!)
+                ////                                questionList.append(questionText!)
+                //                            }
+                //                        }
+                ////                        problemsDict[title!] = questionList
+                ////                        choicesDict[title!] = choicesList
+                ////                        answersDict[title!] = answerIndexList
+                //                    }
+                ////                    self.subjects.append(title!)
+                ////                    subjectDescription.append(description!)
+                //                }
+                //            }
+                //        }
+            }
+            catch {
+                print("Something went boom")
+            }
+        }
+        session.resume()
+    }
+    
+    
+    func postComment(_ reviewUser: String!, _ reviewCardName: String!, _ commentUser: String!, _ description: String!) {
+        var urlString = POST_COMMENT_URL + reviewCardName.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)! + "&reviewAuthor=" + reviewUser.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)! + "&commentAuthor=" + commentUser.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)! + "&comment=" + description.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!
         print(urlString)
         let url = URL(string: urlString)!
         let session = URLSession.shared.dataTask(with: url) {
